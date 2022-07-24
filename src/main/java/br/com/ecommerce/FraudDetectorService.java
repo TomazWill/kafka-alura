13a26e267e62ce1564af5abeb97df7a4b05c1419
Willian Tomaz <willianlima28@hotmail.com>
2022-07-24 13:04:51 -0300

    feat: Deserialização customizada e outras customizações

--- src/main/java/br/com/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/ecommerce/FraudDetectorService.java
@@ -2,20 +2,25 @@ package br.com.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorService {
     public static void main(String[] args) {
 
         var fraudService = new FraudDetectorService();
-        try(var service = new KafkaService(
+        try(var service = new KafkaService<>(
                 FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudService::parse
+                fraudService::parse,
+                Order.class,
+                Map.of()
         )) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("----------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
